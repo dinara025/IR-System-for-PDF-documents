3f8096df59bb8a2610ef2b27b38ca6af83017551
Nimsi Dharmaratne <nimsidinara@gmail.com>
2024-12-19 14:08:02 +0530

    add the requirments

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GenAI project',
+    version= '0.0.0',
+    author= 'Nimsi',
+    author_email= 'nimsidinara@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
